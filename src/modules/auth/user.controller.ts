46d9b9962ad572d431d1e3d86906954c0f4df1d6
tuongdanghung <tuongdanghung37@gmail.com>
2024-03-19 15:11:09 +0700

    change password

---
+++ src/modules/auth/user.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Put, Body, UseGuards } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import { UserServices } from './user.service';
+import { ChangePasswordDTO } from './dto/auth.dto';
+import { SharedDataService } from 'src/shared/middlewares/shareData.service';
+import { CheckAuthenGuard } from 'src/shared/guards/authen.guard';
+dotenv.config();
+@Controller(`${process.env.API_KEY}/user`)
+export class UserController {
+    constructor(
+        private readonly authService: UserServices,
+        private sharedDataService: SharedDataService,
+    ) { }
+
+    @Put('/change-password')
+    @UseGuards(CheckAuthenGuard)
+    changePassword(@Body() changePasswordController: ChangePasswordDTO): Promise<any> {
+        const currentToken = this.sharedDataService.getCurrentToken();
+        const data = { ...changePasswordController, email: currentToken.dataGenerateToken.email }
+        const result = this.authService.changePassword(data);
+        return result;
+    }
+}
