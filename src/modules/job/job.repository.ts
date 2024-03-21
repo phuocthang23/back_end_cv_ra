be200218d18577d73e26abc050f0c3fd07379bcd
tuongdanghung <tuongdanghung37@gmail.com>
2024-03-21 16:01:39 +0700

    edit job and get one job
    fix issue
    
    update feature pagination search
    
    update

--- src/modules/job/job.repository.ts
+++ src/modules/job/job.repository.ts
@@ -1,9 +1,8 @@
 import { JobEntity } from './entities/job.entity';
-import { Repository } from 'typeorm';
+import { Repository, ILike } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JobDTO } from './dto/job.dto';
 import { plainToClass } from 'class-transformer';
-
 export class JobRepository {
   constructor(
     @InjectRepository(JobEntity)
@@ -13,14 +12,26 @@ export class JobRepository {
   async createJob(data: JobDTO): Promise<any> {
     const jobEntity = plainToClass(JobEntity, data);
     this.jobRepository.create(jobEntity);
-    await this.jobRepository.save(jobEntity);
-    return {
-      message: 'Created job successfully',
-    };
+    return await this.jobRepository.save(jobEntity);
   }
 
-  async getAllRoles(): Promise<{ data: any }> {
-    const data = await this.jobRepository.find();
-    return { data };
+  async getAllJob(title: string, limit: number, skip: number) {
+    const data = await this.jobRepository.find({
+      where: title ? { title: ILike(`%${title}%`) } : {},
+      skip,
+      take: limit,
+    });
+    return data;
+  }
+
+
+  async getOneJob(id: number): Promise<any> {
+    return await this.jobRepository.findOne({
+      where: { id }
+    });
+  }
+
+  async updateJob(data: any, id: number): Promise<any> {
+    return await this.jobRepository.update(id, data);
   }
 }
--- src/modules/job/job.service.ts
+++ src/modules/job/job.service.ts
@@ -4,7 +4,7 @@ import { JobDTO } from './dto/job.dto';
 
 @Injectable()
 export class JobServices {
-  constructor(private jobService: JobRepository) { }
+  constructor(private jobRepository: JobRepository) { }
   private convertDateFormat(dateString: string): string {
     const [day, month, year] = dateString.split('/');
     return `${year}-${month}-${day}`;
@@ -21,13 +21,44 @@ export class JobServices {
       companyId: req.companyId,
     };
     try {
-      return await this.jobService.createJob(convertData);
+      await this.jobRepository.createJob(convertData);
+      return {
+        message: 'Created job successfully',
+      };
     } catch (error) {
       throw new HttpException('Failed to create job', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
-  getAllRoles(): Promise<{ data: any }> {
-    return this.jobService.getAllRoles();
+  async getAllJob(title: string, page: number, limit: number) {
+    const skip = (page - 1) * limit;
+    return await this.jobRepository.getAllJob(title, limit, skip);
+  }
+
+  async getOneJob(id: number): Promise<any> {
+    return await this.jobRepository.getOneJob(id);
+  }
+
+  async updateJob(data: any, id: number): Promise<any> {
+    const response = await this.jobRepository.getOneJob(id)
+    if (response === null) {
+      throw new HttpException(
+        'Job not found',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+    try {
+      const result = await this.jobRepository.updateJob(data, id)
+      if (result) {
+        return {
+          message: 'Updated job successfully',
+        };
+      }
+    } catch (error) {
+      throw new HttpException(
+        'User not found',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
