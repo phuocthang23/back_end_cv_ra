59ad386326ab58b493b795fbc4611aaaf6a1b193
HungTD <tuongdanghung2@gmail.com>
2024-03-27 11:31:44 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,6 @@ async function bootstrap() {
   });
   app.useGlobalPipes(new ValidationPipe());
   const PORT = process.env.PORT_URL || 8080;
-  console.log(PORT);
   await app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 }
 bootstrap();
